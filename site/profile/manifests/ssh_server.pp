89cab024e023bd703a755008f6a2c7ddac3e392e
gwllearn <82742144+gwllearn@users.noreply.github.com>
2021-04-19 22:49:37 -0500

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,18 @@
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@master.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEj4tVYlIg15IUBK2icVGmDBXUGSTqjeTl96EKyDZDNYMhUq/Ep3u7CDSt34MPsH9oWIqXmz2MF4KoAtmo
+/nFLabDh4tucn4mxGSKGVFhuwvNSSR3SOsvPSN7OqaHTY+2QlBOAgC8amat050FCGHOKllhtOLEMONPYM093Fx4pNhNDeyXLJCtBsyNd2EiepK0tJc0rHjFc
+OnVUlINoXIQvUyS9cqqcajgQsffomg9mVMZ5zP0ICSKCIqj1yYDFHXeUwYi2hpo3yfb1D1HwYcLx9OfmEkwbOOeeZHhqVBOL8mWBoJ29M7g6zPAoFQhMaK8U
+NrN8vjCRCXVcJ45LGsmb',
+	}  
+}
