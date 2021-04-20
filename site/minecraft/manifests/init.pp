2dbad60f82a56e5a14e16f08f4555e823d6ca8a0
gwllearn <82742144+gwllearn@users.noreply.github.com>
2021-04-20 01:45:00 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
   file { '/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    before => Service['minecraft'],
   }
   package { 'java':
     ensure => present,
@@ -20,5 +21,6 @@ class minecraft {
   service { 'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],file['/opt/minecraft/eulta.txt'],file['/etc/systemd/system/minecraft.service']],
   }
 }
